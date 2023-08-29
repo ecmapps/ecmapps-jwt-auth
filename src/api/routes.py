8cb8dba7f31df49d22a9e7a39d80e801588e6fb2
ecmapps <72906286+ecmapps@users.noreply.github.com>
2023-08-29 00:48:25 +0000

    User creation and login functional

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,13 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
-
+#Agregar al boilerplate
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +20,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def createUser():
+    data = request.get_json(force=True)
+    new_user=User()
+    secure_password = bcrypt.generate_password_hash((data["password"]), 10).decode("utf-8")
+    new_user.email = data["email"]
+    new_user.password = secure_password
+    new_user.is_active = True
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "User created successfully"}), 201
+
+@api.route('/login', methods=["POST"])
+def login_user():
+    data = request.get_json(force=True)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Incorrect user or password"}), 401
+    
+    token = create_access_token(identity = data["id"], additional_claims={"role":"admin"})
+    return jsonify({"msg": "Login successful!", "token":token}),200
